b7100c35391724e15fe369d5ff27256d9485ed2c
maru801 <rurigames9@gmail.com>
2024-06-16 13:44:10 -0400

    Changed the logic for the chapter error script to make a copy of the daily log file instead.

--- chapter-error-checker-jellyfin-v10.9.6.sh
+++ chapter-error-checker-jellyfin-v10.9.6.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#scriptLogLocationPathOnly=test/
+scriptLogLocationPathOnly=test/
 scriptLogLocation=test/chapter-error-log.txt
 scriptTempLogLocation=test/temp-chapter-error-log.txt
 
 pathToJellyLogs="path/to/file" ## Make sure to not end this path with a foward slash "/"
 todaysLog=$(date +"log_%Y%m%d.log")
-
-fullPathToTodaysLog=$pathToJellyLogs/$todaysLog
 
 if [ "$1" == "-c" ]; then
     echo "Delete the \"chapter-error-log.txt\" file?"
@@ -24,13 +22,16 @@ elif [ "$1" == "-C" ]; then
 elif [ "$1" == "-d" ]; then
     cat $scriptLogLocation
 elif [ "$1" == "-h" ]; then
+    cp $pathToJellyLogs/$todaysLog $scriptLogLocationPathOnly/copyOfTodaysLog.log
+    fullPathToTodaysCopyLog=$scriptLogLocationPathOnly/copyOfTodaysLog.log
+    
     echo "--------------------------------" >> $scriptTempLogLocation
     date >> $scriptTempLogLocation
     echo "--------------------------------" >> $scriptTempLogLocation
 
     echo "Error: Jellyfin chapter extraction stopped for:" >> $scriptTempLogLocation
     echo "" >> $scriptTempLogLocation
-    awk '{print $0}' <(awk -F "|" '{print $2}' <(grep -i "stopping chapter extraction for" $fullPathToTodaysLog | sed 's/topping chapter extraction for \"/topping chapter extraction for \|\"/g')) | sort | uniq >> $scriptTempLogLocation
+    awk '{print $0}' <(awk -F "|" '{print $2}' <(grep -i "stopping chapter extraction for" $fullPathToTodaysCopyLog | sed 's/topping chapter extraction for \"/topping chapter extraction for \|\"/g')) | sort | uniq >> $scriptTempLogLocation
 
     echo "" >> $scriptTempLogLocation
     echo "" >> $scriptTempLogLocation
@@ -41,12 +42,14 @@ elif [ "$1" == "-h" ]; then
     fi
 
     rm $scriptTempLogLocation
+    rm $fullPathToTodaysCopyLog
 else
     echo "This script will check Jellyfin's current daily logfile for any chapter errors and do the following:"
     echo "   1. Print out any found chapter errors"
     echo "   2. Store any found chapter errors at $scriptLogLocation"
     echo ""
-    echo "Note: Jellyfin make a new logfile daily. This script only checks today's logfile."
+    echo "Note: Jellyfin make a new logfile daily. This script only checks today's logfile by making a copy of it and checking it after."
+    echo "Therefore for best results, make sure to run this script just before the day ends. Note that Jellyfin's log cleanup task only deletes log files older than 3 days."
     echo ""
     echo "Flags available to use with this script:"
     echo "----------------------------------------"
@@ -58,13 +61,16 @@ else
     read userExitChoice
 
     if [ "$userExitChoice" == "Y" ]; then
+        cp $pathToJellyLogs/$todaysLog $scriptLogLocationPathOnly/copyOfTodaysLog.log
+        fullPathToTodaysCopyLog=$scriptLogLocationPathOnly/copyOfTodaysLog.log
+        
         echo "--------------------------------" >> $scriptTempLogLocation
         date >> $scriptTempLogLocation
         echo "--------------------------------" >> $scriptTempLogLocation
 
         echo "Error: Jellyfin chapter extraction stopped for:" >> $scriptTempLogLocation
         echo "" >> $scriptTempLogLocation
-        awk -F "|" '{print $2}' <(grep -i "stopping chapter extraction for" $fullPathToTodaysLog | sed 's/topping chapter extraction for \"/topping chapter extraction for \|\"/g') | sort | uniq >> $scriptTempLogLocation
+        awk -F "|" '{print $2}' <(grep -i "stopping chapter extraction for" $fullPathToTodaysCopyLog | sed 's/topping chapter extraction for \"/topping chapter extraction for \|\"/g') | sort | uniq >> $scriptTempLogLocation
 
         echo "" >> $scriptTempLogLocation
         echo "" >> $scriptTempLogLocation
@@ -76,6 +82,7 @@ else
         fi
 
         rm $scriptTempLogLocation
+        rm $fullPathToTodaysCopyLog
         echo "...Exiting..."
     else
         echo "...Exiting..."
